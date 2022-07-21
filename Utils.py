44b0861319bc878aac5b9b268ea5dd1d9e86b8de
LeonidVolohov <leka.volohov@gmail.com>
2022-07-21 13:24:33 +0300

    QTreeView:  - Utils:   - handled exceptions in getAbsFilePath() method   - added fileNameMatch() function for checking if given name from QTreeView is acceptable for further working with it

--- Utils.py
+++ Utils.py
@@ -1,15 +1,28 @@
 import os
+import re
 
 class Utils():
 	def __init__(self):
 		pass
 
 	def getAbsFilePath(self, fileName):
-		scriptPath = os.path.abspath(__file__) # i.e. /path/to/dir/foobar.py
-		scriptDir = os.path.split(scriptPath)[0] #i.e. /path/to/dir/
-		absFilePath = os.path.join(scriptDir, fileName)
+		try:
+			scriptPath = os.path.abspath(__file__) # i.e. /path/to/dir/foobar.py
+			scriptDir = os.path.split(scriptPath)[0] #i.e. /path/to/dir/
+			absFilePath = os.path.join(scriptDir, fileName)
 
-		return absFilePath
+			return absFilePath
+		except Exception as exception:
+			return str(exception)
+
+
+	def fileNameMatch(self, fileName):
+		try:
+			return True if re.search("\.json$", fileName) else False
+		except Exception as exception:
+			return False
+
+		return False
 
 
 def main():
