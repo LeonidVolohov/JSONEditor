f61626c2de5cfa930a7676547f31e86f71ce3b15
LeonidVolohov <leka.volohov@gmail.com>
2022-07-26 13:12:07 +0300

    Configuration:  - small changes p.2

--- utils/config/config.py
+++ utils/config/config.py
@@ -3,24 +3,24 @@ from configparser import ConfigParser
 configObject = ConfigParser()
 
 configObject["QTreeView"] = {
-	"setAlternatingRowColors": 	"False",
-	"setAnimated": 				"False",
+	"setAlternatingRowColors":			"False",
+	"setAnimated":						"False",
 	
-	"expandAll": 				"False",
-	"expandToDepth": 			"-2", 				# -2: Dont Expand, -1: ExpandAll, 0: Zero level, ...
+	"expandAll":						"False",
+	"expandToDepth":					"-2", 				# -2: Dont Expand, -1: ExpandAll, 0: Zero level, ...
 }
 
 configObject["Other"] = {
-	"defaultJsonFileName": 		"config_apak.json"	# default: ""
+	"defaultJsonFileName":				"config_apak.json"	# default: ""
 }
 
 configObject["Language"] = {
-	"defaultLanguage": 			"en",				# Language for GUI
-	"writeToJsonLanguage": 		"en" 				# Language for writing back to JSON-file
+	"defaultLanguage":					"en",				# Language for GUI
+	"writeToJsonLanguage":				"en" 				# Language for writing back to JSON-file
 }
 
 configObject["MainWindow"] = {
-	"showMaximized": 			"False"
+	"showMaximized":					"False"
 }
 
 with open('config.ini', 'w') as config:
