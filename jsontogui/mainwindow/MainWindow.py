225631ae00d297411db0ce9e0ebf5deedf8983eb
LeonidVolohov <leka.volohov@gmail.com>
2022-08-09 16:17:28 +0300

    GUIDecoration:  - added implementation of setStyleSheet() for QTreeView in MainWindow.py

--- jsontogui/mainwindow/MainWindow.py
+++ jsontogui/mainwindow/MainWindow.py
@@ -186,7 +186,7 @@ class MainWindow(QMainWindow):
 
         self.model = QJsonTreeModel()
         self.tree_view.setModel(self.model)
-        self.tree_view.setColumnWidth(0, 400)
+        self.tree_view.setColumnWidth(0, 512)
         self.tree_view.setContextMenuPolicy(Qt.CustomContextMenu)
         self.tree_view.customContextMenuRequested.connect(self.open_right_click_menu)
 
@@ -194,6 +194,76 @@ class MainWindow(QMainWindow):
             Utils().string_to_boolean(CONFIG_OBJECT.get("QTreeView", "set_alternating_row_colors")))
         self.tree_view.setAnimated(
             Utils().string_to_boolean(CONFIG_OBJECT.get("QTreeView", "set_animated")))
+
+        self.tree_view.setStyleSheet("""
+            QTreeView {
+                border: 3px solid;
+                border-color: #678DB2;
+                /*alternate-background-color: yellow;
+                show-decoration-selected: 100; */
+                background: #FFFFFF;
+            }
+
+            QTreeView::item {
+                border: 1px solid #D9D9D9;
+                border-top-color: transparent;
+                border-bottom-color: transparent;
+                border-left-color: transparent;
+            }   
+            
+            QTreeView::item:children {
+                height: 28px;
+            }
+
+            QTreeView::item:hover {
+                background: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #E5F3FF, stop: 1 #E5F3FF);
+                border: 1px solid #bfcde4;
+            }
+
+            QTreeView::item:selected {
+                border: 1px solid #678DB2;
+                color: #000000;
+            }
+
+            QTreeView::item:selected:active{
+                background: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #CCE8FF, stop: 1 #CCE8FF);
+            }
+        
+            /*
+            QTreeView::branch {
+                background-color: white;
+            } */
+            
+            /*
+            QTreeView::branch:closed:has-children {
+                image: url("utils/images/treeview/branch-closed.png");
+            }
+
+            QTreeView::branch:has-siblings:!adjoins-item {
+                border-image: url("utils/images/treeview/vline.png") 0;
+            }
+
+            QTreeView::branch:has-siblings:adjoins-item {
+                border-image: url("utils/images/treeview/branch-more.png") 0;
+            }
+
+            QTreeView::branch:!has-children:!has-siblings:adjoins-item {
+                border-image: url("utils/images/treeview/branch-end.png") 0;
+            }
+
+            QTreeView::branch:has-children:!has-siblings:closed,
+            QTreeView::branch:closed:has-children:has-siblings {
+                border-image: none;
+                image: url("utils/images/treeview/branch-closed.png)";
+            }
+
+            QTreeView::branch:open:has-children:!has-siblings,
+            QTreeView::branch:open:has-children:has-siblings  {
+                border-image: none;
+                image: url("utils/images/treeview/branch-open.png");
+            } 
+            */
+        """)
 
         self.model.clear()
         self.model.load(self.json_text)
@@ -676,7 +746,7 @@ class MainWindow(QMainWindow):
                 item_type_dict_or_list = \
                     self.model.getItem(self.tree_view.selectedIndexes()[1]).type is dict or \
                     self.model.getItem(self.tree_view.selectedIndexes()[1]).type is list
-                
+
                 action_add_item.menuAction().setVisible(False)
                 action_add_dictionary.setVisible(False)
                 action_add_list.setVisible(False)
@@ -689,6 +759,8 @@ class MainWindow(QMainWindow):
                     action_add_item.menuAction().setVisible(True)
                     action_add_dictionary.setVisible(True)
                     action_add_list.setVisible(True)
+                    if not empty_value and Utils().file_name_match(file_name, "json"):
+                        action_tree_item_open_json_file.setVisible(True)
                 elif is_parent_root and item_type_dict_or_list:
                     action_add_item.menuAction().setVisible(True)
                     action_add_dictionary.setVisible(True)
