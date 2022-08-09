91e0710e2d36e94de503fa3e1a72290c339630f7
LeonidVolohov <leka.volohov@gmail.com>
2022-08-09 16:16:21 +0300

    GUIDecoration:  - added implementation of setStyleSheet() for QHeaderView in main.py

--- jsontogui/main.py
+++ jsontogui/main.py
@@ -17,4 +17,14 @@ if __name__ == '__main__':
         json_file_name=CONFIG_OBJECT.get("Other", "default_json_file_name"),
         show_maximized=Utils().string_to_boolean(CONFIG_OBJECT.get("MainWindow", "show_maximized")))
 
+    APPLICATION.setStyleSheet("""
+        QHeaderView::section {
+            /* background-color: #678DB2; */
+            color: black;
+            height: 28px;
+            font-size: 16px;
+            font-family: courier;
+        }
+    """)
+
     sys.exit(APPLICATION.exec_())
